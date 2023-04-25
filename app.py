5191fbc8829327bbe37634be1ac5effe450af470
Oleg Babin <babin.architect@gmail.com>
2023-04-26 00:22:32 +0300

    update for lesson

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from services import SocialNetworkScraper
 
 if __name__ == "__main__":
     service = SocialNetworkScraper()
-    driver = service.create_driver()
+
+    title = "Test automated post"
+    content = "Test automated post content"
+    # service.social_network_login()
+    service.social_network_add_post(title, content)
+    # driver = service.create_driver()
     print('done')
--- services.py
+++ services.py
@@ -6,18 +6,65 @@ from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 
 import config
-#  from decorators import social_login_required
-
-
+from decorator import social_login_required
 
 
 class SocialNetworkScraper:
-    def __init__(self):
-        self.driver = None
+
+    BASE_URL = f"http://{config.SOCIAL_NETWORK_HOST}:{config.SOCIAL_NETWORK_PORT}"
+    LOGIN_URL = f"{BASE_URL}/auth/login"
+    BLOG_URL = f"{BASE_URL}/user/blog"
+
+    def __init__(self, driver=None):
+        self.driver = driver or self.create_driver()
+        self.is_logged_in = False
 
     def create_driver(self):
+        """
+        Create chrome driver instance
+        """
         try:
-            self.driver = webdriver.Chrome(executable_path=config.CHROME_DRIVER_PATH)
+            options = Options()
+            options.add_argument("--start-maximized")
+            self.driver = webdriver.Chrome(executable_path=config.CHROME_DRIVER_PATH, options=options)
             return self.driver
         except Exception as e:
             print(e.args)
+
+    def social_network_login(self):
+        # create driver & navigate to login page
+        # driver = self.create_driver()
+        # driver.get(self.LOGIN_URL)
+        self.driver.get(self.LOGIN_URL)
+
+        username_elem = self.driver.find_element(By.XPATH, "//div[@class='form-group']/input[@id='username']")
+        # username_elem1 = driver.find_element(By.ID, "username")
+        username_elem.send_keys(config.SOCIAL_NETWORK_LOGIN)
+
+        password_elem = self.driver.find_element(By.XPATH, "//div[@class='form-group']/input[@id='password']")
+        password_elem.send_keys(config.SOCIAL_NETWORK_PASSWORD)
+
+        password_elem.send_keys(keys.Keys.ENTER)
+        self.is_logged_in = True
+
+
+    @social_login_required
+    def social_network_add_post(self, title, content):
+
+        self.driver.get(self.BLOG_URL)
+        time.sleep(1)
+
+        title_elem = self.driver.find_element(By.ID, "title")
+        title_elem.send_keys(title)
+        time.sleep(1)
+
+        content_elem = self.driver.find_element(By.ID, "content")
+        content_elem.send_keys(content)
+        time.sleep(1)
+
+        create_post_elem = self.driver.find_element(By.XPATH, "//form/button[@type='submit']")
+        create_post_elem.click()
+        time.sleep(1)
+
+        return self.driver
+
