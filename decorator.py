f27bbf5dca09db3c59e733e03b2976b36e7f5330
Oleg Babin <babin.architect@gmail.com>
2023-04-26 17:04:24 +0300

    update decorator

--- decorator.py
+++ decorator.py
@@ -15,3 +15,18 @@ def social_login_required(func):
         return func(self, *args, **kwargs)
 
     return wrapper
+
+def social_reg_required(func):
+    @wraps(func)
+    def wrapper(self, * args, ** kwargs):
+
+        if self.is_reg_in:
+            return func(self, *args, **kwargs)
+
+        # If not logged in, perform social login
+        self.social_network_register()
+
+        # Call the original function
+        return func(self, *args, **kwargs)
+
+    return wrapper
