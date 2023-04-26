779d54842a0449e620ee2720848f071f2651a7c8
Oleg Babin <babin.architect@gmail.com>
2023-04-26 17:05:07 +0300

    update app

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from services import SocialNetworkScraper
 if __name__ == "__main__":
     service = SocialNetworkScraper()
 
-    title = "Test automated post"
-    content = "Test automated post content"
-    # service.social_network_login()
+    title = "Selenium post"
+    content = "Автоматический пост, тепрь я смогу взламівать Пентагон :)"
     service.social_network_add_post(title, content)
-    # driver = service.create_driver()
     print('done')
