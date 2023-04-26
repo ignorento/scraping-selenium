c624184f56d15093b08aa8eb19fafbd848983574
Oleg Babin <babin.architect@gmail.com>
2023-04-26 17:05:17 +0300

    update services

--- services.py
+++ services.py
@@ -6,18 +6,21 @@ from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 
 import config
-from decorator import social_login_required
+from decorator import social_login_required, social_reg_required
 
 
 class SocialNetworkScraper:
 
     BASE_URL = f"http://{config.SOCIAL_NETWORK_HOST}:{config.SOCIAL_NETWORK_PORT}"
+    REGISTER_URL = f"{BASE_URL}/auth/register"
     LOGIN_URL = f"{BASE_URL}/auth/login"
     BLOG_URL = f"{BASE_URL}/user/blog"
 
     def __init__(self, driver=None):
         self.driver = driver or self.create_driver()
         self.is_logged_in = False
+        self.is_reg_in = False
+
 
     def create_driver(self):
         """
@@ -31,7 +34,52 @@ class SocialNetworkScraper:
         except Exception as e:
             print(e.args)
 
-    def social_network_login(self):
+
+
+    def social_network_register(self):
+        self.driver.get(self.REGISTER_URL)
+        time.sleep(1)
+
+        reg_username = self.driver.find_element(By.XPATH, "//form/div/input[@id='username']")
+        reg_username.send_keys(config.SOCIAL_NETWORK_LOGIN)
+        time.sleep(1)
+
+        reg_email = self.driver.find_element(By.XPATH, "//form/div/input[@id='email']")
+        reg_email.send_keys(config.SOCIAL_NETWORK_EMAIL)
+        time.sleep(1)
+
+        reg_password = self.driver.find_element(By.XPATH, "//form/div/input[@id='password']")
+        reg_password.send_keys(config.SOCIAL_NETWORK_PASSWORD)
+        time.sleep(1)
+
+        reg_password_confirm = self.driver.find_element(By.XPATH, "//form/div/input[@id='confirm_password']")
+        reg_password_confirm.send_keys(config.SOCIAL_NETWORK_PASSWORD)
+        time.sleep(1)
+
+        reg_first_name = self.driver.find_element(By.XPATH, "//form/div/input[@id='first_name']")
+        reg_first_name.send_keys(config.SOCIAL_NETWORK_FIRST_NAME)
+        time.sleep(1)
+
+        reg_last_name = self.driver.find_element(By.XPATH, "//form/div/input[@id='last_name']")
+        reg_last_name.send_keys(config.SOCIAL_NETWORK_LAST_NAME)
+        time.sleep(1)
+
+        reg_linkedin = self.driver.find_element(By.XPATH, "//form/div/input[@id='linkedin']")
+        reg_linkedin.send_keys(config.SOCIAL_NETWORK_LINKEDIN)
+        time.sleep(1)
+
+        reg_facebook = self.driver.find_element(By.XPATH, "//form/div/input[@id='facebook']")
+        reg_facebook.send_keys(config.SOCIAL_NETWORK_FACEBOOK)
+        time.sleep(1)
+
+        reg_facebook.send_keys(keys.Keys.ENTER)
+        time.sleep(1)
+
+        self.is_reg_in = True
+
+
+
+    def social_network_login(self) -> object:
         # create driver & navigate to login page
         # driver = self.create_driver()
         # driver.get(self.LOGIN_URL)
@@ -47,7 +95,7 @@ class SocialNetworkScraper:
         password_elem.send_keys(keys.Keys.ENTER)
         self.is_logged_in = True
 
-
+    @social_reg_required
     @social_login_required
     def social_network_add_post(self, title, content):
 
@@ -66,5 +114,19 @@ class SocialNetworkScraper:
         create_post_elem.click()
         time.sleep(1)
 
-        return self.driver
+        return self.social_network_add_like()
 
+
+    def social_network_add_like(self):
+
+        like = self.driver.find_element(By.XPATH, "//div[@class='btn-group']/a[@class='btn btn-sm btn-outline-success']")
+        like.click()
+
+        return self.social_network_logout()
+
+
+    def social_network_logout(self):
+        logout = self.driver.find_element(By.XPATH, "//a[@href='/auth/logout']")
+        logout.click()
+
+        return self.driver
